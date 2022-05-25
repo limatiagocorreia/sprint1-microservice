3d8dd9f2bcff74d879be6d6f89f844478005c23e
lima-tiago <lima.tiagocorreia@gmail.com>
2022-05-24 22:34:23 -0300

    first commit

---
+++ sprintMicro/src/main/java/br/com/fiap/sprintMicro/SprintMicroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprintMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintMicroApplication.class, args);
+	}
+
+}
